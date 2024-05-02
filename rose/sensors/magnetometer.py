c9aceaba264998c0048cc54d77e056dd98688a61
William Fauteux <142356794+gal1leo31@users.noreply.github.com>
2024-05-02 15:48:25 -0400

    change bus addresses
    Device address and data addresses differ from the HMC5883L manufactured by Honeywell (now discontinued) and the HMC5883L, originally QMC5883L, which is manufactured by a Chinese company.

--- rose/sensors/magnetometer.py
+++ rose/sensors/magnetometer.py
@@ -11,9 +11,9 @@ import math
 import numpy as np
 
 # Some MPU6050 Registers and their Address
-register_a     = 0              # Address of Configuration register A
-register_b     = 0x01           # Address of configuration register B
-register_mode  = 0x02           # Address of mode register
+register_a     = 0x0b           # Address of Configuration register A
+register_b     = 0x09           # Address of configuration register B
+#register_mode  = 0x02           # Address of mode register
 
 # Select the correct i2c bus for this revision of Raspberry Pi
 revision = ([l[12:-1] for l in open('/proc/cpuinfo','r').readlines() if l[:8]=="Revision"]+['0000'])[0]
@@ -21,20 +21,28 @@ revision = ([l[12:-1] for l in open('/proc/cpuinfo','r').readlines() if l[:8]=="
 # MPU6050 class
 class Magnetometer:
         
-        def __init__(self, bus = smbus.SMBus(1 if int(revision, 16) >= 4 else 0), device_address = 0x1e, x_axis_h = 0x03, y_axis_h = 0x07, z_axis_h = 0x05):
+        def __init__(self, bus = smbus.SMBus(1 if int(revision, 16) >= 4 else 0), device_address = 0x0d, x_axis_h = 0x00, y_axis_h = 0x02, z_axis_h = 0x04):
+                if bus is None:
+                # Select the correct i2c bus for this revision of Raspberry Pi
+                        revision = (
+                                [l[12:-1] for l in open('/proc/cpuinfo','r').readlines() \
+                                if l[:8]=="Revision"]+['0000']
+                        )[0]
+                        bus = smbus.SMBus(1 if int(revision, 16) >= 4 else 0)
+                
                 self.bus = bus
                 
                 # HMC5883L magnetometer device address
                 self.device_address = device_address
 
                 # Write to Configuration Register A
-                bus.write_byte_data(device_address, register_a, 0x70)
+                bus.write_byte_data(device_address, register_a, 0x01)
 
                 # Write to Configuration Register B for gain
-                bus.write_byte_data(device_address, register_b, 0xa0)
+                bus.write_byte_data(device_address, register_b, 0x1d)
 
                 # Write to mode Register for selecting mode
-                bus.write_byte_data(device_address, register_mode, 0)
+                #bus.write_byte_data(device_address, register_mode, 0)
 
                 # Axis
                 self.x_axis = self._rawData(x_axis_h)
@@ -45,7 +53,7 @@ class Magnetometer:
                 # Read raw 16-bit value
                 high = self.bus.read_byte_data(self.device_address, address)
                 low = self.bus.read_byte_data(self.device_address, address + 1)
-
+                print(high,low)
                 # Concatenate higher and lower value
                 value = ((high << 8) | low)
 
@@ -60,7 +68,7 @@ class Magnetometer:
                 x = self.x_axis
                 y = self.y_axis
                 z = self.z_axis
-
+                print(x,y,z)
                 heading = math.atan2(y, x)
 
                 # Due to declination check for > 360 degree
@@ -72,15 +80,15 @@ class Magnetometer:
                         heading = heading + 2 * (np.pi)
 
                 # Convert into angle
-                heading_angle = int(heading * 180/(np.pi))
+                heading_angle = heading * 180/(np.pi) # int(heading * 180/(np.pi))
 
-                return heading_angle
+                return {'heading' : heading_angle}
 
         @property
         def heading(self):
-                return self._getData()
+                return self._getData()['heading'] 
 
 if __name__=="__main__":
         Magnetometer = Magnetometer()
         
-        print(f"Heading angle = {Magnetometer.heading} degrees")
+        print("Heading angle = %.4f degrees" % Magnetometer.heading)
